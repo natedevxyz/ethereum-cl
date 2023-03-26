ca7e18715f51c36bda845d85bccf059f66528324
nate-d3v <nate.d3v@gmail.com>
2023-03-25 23:50:57 -0300

    Finish first basic concept descentralizacion

--- pages/conocimiento/descentralizacion.js
+++ pages/conocimiento/descentralizacion.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import NextLink from 'next/link';
 import Wrapper from '../../components/Wrapper';
 import { SlArrowLeft } from 'react-icons/sl';
-import { Flex, Text, Link, Box } from '@chakra-ui/react';
+import { Flex, Text, Link, Box, Image } from '@chakra-ui/react';
 
 export default function Descentralizacion() {
 	return (
@@ -33,7 +33,7 @@ export default function Descentralizacion() {
 					<Text fontSize={{ base: '2xl', md: '4xl' }} color="#4D81F7" mb={5}>
 						¿Por qué es importante la descentralización?
 					</Text>
-					<Flex flexDirection={{ base: 'column', md: 'row' }}>
+					<Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
 						<Box w={{ base: '100%', md: '50%' }}>
 							<Text textAlign="justify" mb={5} fontSize={{ md: 'lg' }}>
 								Intenta hacer memoria de los pasos que recién seguiste para
@@ -44,39 +44,48 @@ export default function Descentralizacion() {
 								búsqueda. El más utilizado a nivel mundial es Google, algo tan
 								normal en nuestras vidas que tenemos un verbo para eso:
 								"googlear". Ocupamos servicios como estos todos los días y no
-								pagamos nada, ¿cómo lo hacen estas grandes empresas tecnológicas
-								para generar ingresos?.
+								pagamos nada, ¿cómo lo hacen estas grandes compañías para
+								generar ingresos?.
 							</Text>
 							<Text textAlign="justify" mb={5} fontSize={{ md: 'lg' }}>
-								Piensa ahora en cuánta información hay de ti en la nube.
-								Correos, conversaciones, fotos, etc. Es innegable que todo esto
-								nos facilita la vida y además a un precio imbatible: gratis.
-								¿Cómo algo así puede ser una desventaja para los usuarios?
+								Piensa ahora cuánta información personal mantienes
+								voluntariamente en estas plataformas. Correos, fotos,
+								conversaciones, etc. Es innegable que todo esto nos facilita la
+								vida y además a un precio imbatible: gratis. ¿Cómo algo así
+								puede ser una desventaja para los usuarios?
 							</Text>
 							<Text textAlign="justify" mb={5} fontSize={{ md: 'lg' }}>
 								Esto podría compararse a ser súbditos de un reino. Este reino
 								ofrece muchos beneficios sabiendo que mientras más súbditos
-								tenga mejor será la perspectiva económica. Pero la historia ha
-								demostrado que cuando una persona (como un rey) tiene mucho
-								poder, se genera un desequilibrio que no beneficia a la mayoría
-								(como los súbditos).
+								tenga, mejores serán sus perspectivas económicas. Pero la
+								historia ha demostrado que cuando una persona (como un rey)
+								tiene mucho poder, se genera un desequilibrio que no beneficia a
+								la mayoría (como los súbditos).
 							</Text>
 							<Text textAlign="justify" mb={5} fontSize={{ md: 'lg' }}>
 								Esta comparación puede parecer una exageración, pero no lo es.
-								Estas compañías tecnológicas tienen como objetivo la
-								rentabilidad, y aunque ese objetivo no es algo malo, si es
-								riesgoso que todo dependa de una persona o un grupo. ¿Qué harías
-								si mañana eliminaran todas tus cuentas?, ¿a quién le
-								reclamarías?.
+								Las compañías tecnológicas como Google tienen como principal
+								prioridad la rentabilidad, y aunque eso no es algo malo, sí hay
+								un conflicto de intereses cuando un grupo pequeño tiene que
+								decidir entre los resultados económicos y el bien de los
+								usuarios.
 							</Text>
 							<Text textAlign="justify" mb={5} fontSize={{ md: 'lg' }}>
-								Hasta el momento este escenario no es un problema para la
-								mayoría de los usuarios. Pero intenta cambiarte de "reino" y te
+								Hasta el momento la centralización no es una preocupación para
+								la mayoría de la gente. Pero intenta cambiarte de "reino" y te
 								darás cuenta de que no es fácil dejar de depender de estas
 								entidades centralizadas. Lo bueno es que hay una tecnología que
 								nos permite seguir beneficiándonos de internet sin los riesgos
 								de la centralización: blockchain.
 							</Text>
+						</Box>
+						<Box mb={5} pl={{ md: 10 }}>
+							<Image
+								position={{ base: 'static', md: 'sticky' }}
+								top="4.5rem"
+								src="/google.jpg"
+								fit="contain"
+							></Image>
 						</Box>
 					</Flex>
 				</Flex>
